a3d0863e6c4430464ae6c18e49e7a30926ac1691
Durva <durvakulkarni3@gmail.com>
2022-06-14 15:32:40 +0530

    Save and Save as Functionality added

--- NotepadApp/src/notepadapp/About.java
+++ NotepadApp/src/notepadapp/About.java
@@ -1,4 +1,5 @@
 package notepadapp;
+import java.awt.Font;
 import javax.swing.*;
 
 public class About extends JFrame
@@ -6,16 +7,24 @@ public class About extends JFrame
     About()
     {
         setTitle("About Notepad");
-        setSize(500,500);
-        setLocation(700,200);
+        setSize(1200,800);
+        setLocation(100,100);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         ImageIcon icon=new ImageIcon(getClass().getResource("notepad.png"));
         setIconImage(icon.getImage());
         
         JLabel iconlabel=new JLabel(new ImageIcon(getClass().getResource("notepad.png")));
+        iconlabel.setBounds(300,220,250,230);
         add(iconlabel);
+        JLabel textlabel=new JLabel("<html>Welcome to Notepad <br> Notepad is a simple text editor for Microsoft Windows and a basic text editing program which enables computer users to create documents <br> All rights reserved@2022<html>");
+        textlabel.setBounds(600,150,400,400);
+        textlabel.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,20));
+        add(textlabel);
+        JLabel extra=new JLabel("<html><html>");
+        extra.setBounds(300,100,250,230);
+        add(extra);
         
-    }
+    }   
     public static void main(String args[])
     {
         About abt=new About();
--- NotepadApp/src/notepadapp/NotepadApp.java
+++ NotepadApp/src/notepadapp/NotepadApp.java
@@ -1,8 +1,21 @@
 package notepadapp;
+import java.awt.FileDialog;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import javafx.scene.text.Text;
 import javax.swing.*;
 
-public class NotepadApp extends JFrame 
+public class NotepadApp extends JFrame implements ActionListener
 {
+    static NotepadApp notepad;
+    Text t;
+    String fileName=null,fileAddress=null;
+    JFileChooser chooser;
+    
     JMenuBar mbar=new JMenuBar();
     JMenu file=new JMenu("File");
     JMenu edit=new JMenu("Edit");
@@ -11,6 +24,7 @@ public class NotepadApp extends JFrame
     JMenuItem newFile=new JMenuItem("New");
     JMenuItem openFile=new JMenuItem("Open");
     JMenuItem saveFile=new JMenuItem("Save");
+    JMenuItem saveAsFile=new JMenuItem("Save As");
     JMenuItem print=new JMenuItem("Print");
     JMenuItem exit=new JMenuItem("Exit");
     
@@ -20,6 +34,7 @@ public class NotepadApp extends JFrame
     JMenuItem selectall=new JMenuItem("Select All");
     
     JMenuItem about=new JMenuItem("About");
+    JTextArea textarea=new JTextArea();
     
     NotepadApp()
     {
@@ -37,6 +52,7 @@ public class NotepadApp extends JFrame
         
         file.add(newFile);
         file.add(openFile);
+        file.add(saveAsFile);
         file.add(saveFile);
         file.add(print);
         file.add(exit);
@@ -47,12 +63,117 @@ public class NotepadApp extends JFrame
         edit.add(selectall);
         
         help.add(about);
+        
+        newFile.addActionListener(this);
+        openFile.addActionListener(this);
+        saveAsFile.addActionListener(this);
+        saveFile.addActionListener(this);
+        print.addActionListener(this);
+        exit.addActionListener(this);
+        
+        cut.addActionListener(this);
+        copy.addActionListener(this);
+        paste.addActionListener(this);
+        selectall.addActionListener(this);
+        
+        about.addActionListener(this);
+        
+        JScrollPane scrollpane=new JScrollPane(textarea);
+        add(scrollpane);
+        scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        
+        textarea.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,22));
+        textarea.setLineWrap(true);
     }
     
     public static void main(String args[])
     {
-        NotepadApp notepad=new NotepadApp();
+        notepad=new NotepadApp();
         notepad.setVisible(true);
         
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()=="newFile")
+        {
+            
+        }
+        else if(e.getSource()=="openFile")
+        {
+            
+        }
+        else if(e.getSource()==saveAsFile)
+        {
+            FileDialog fd = new FileDialog(notepad,"Save As",FileDialog.SAVE);
+            fd.setVisible(true);
+
+            if(fd.getFile()!=null)
+            {
+                    fileName = fd.getFile();
+                    fileAddress = fd.getDirectory();
+            }
+
+            try
+            { 
+                    BufferedWriter br = new BufferedWriter(new FileWriter(fileAddress+fileName));
+                    br.write(notepad.textarea.getText());
+                    br.close();
+            }
+            catch(Exception ie)
+            {
+
+            }
+        }
+        else if(e.getSource()==saveFile)
+        {
+            if(fileName==null)
+		{
+			
+		}
+		else
+		{
+			try
+			{
+				
+                            BufferedWriter br = new BufferedWriter(new FileWriter(fileAddress+fileName));
+                            br.write(notepad.textarea.getText());
+                            br.close();
+                            notepad.setTitle(fileName);
+			}
+			catch(IOException ie)
+			{
+				
+			}
+		}
+        }
+        else if(e.getSource()=="print")
+        {
+            
+        }
+        else if(e.getSource()==exit)
+        {
+            System.exit(0);
+        }
+        else if(e.getSource()=="cut")
+        {
+            
+        }
+        else if(e.getSource()=="copy")
+        {
+            
+        }
+        else if(e.getSource()=="paste")
+        {
+            
+        }
+        else if(e.getSource()=="selectall")
+        {
+            
+        }
+        else if(e.getSource()=="about")
+        {
+            
+        }
     }
 }
